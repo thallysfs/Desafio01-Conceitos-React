a3ee99c5a88546fc18ddb86501b33d3b440ede5d
thallysfs <thallys@hotmail.com>
2021-09-20 14:55:13 -0300

    primeiro desafio react.js

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -16,14 +16,34 @@ export function TaskList() {
 
   function handleCreateNewTask() {
     // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
+    if(newTaskTitle){
+      setTasks((prevTasks)=>[
+        ...prevTasks,
+        {
+          id: Math.trunc(Math.random() * 1000),
+          title: newTaskTitle,
+          isComplete: false
+        },
+      ])
+    }
+
   }
 
   function handleToggleTaskCompletion(id: number) {
     // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+    setTasks(
+      tasks.map((task)=>
+        task.id == id ? {...task, isComplete: !task.isComplete}: task
+      )
+    )
+
   }
 
   function handleRemoveTask(id: number) {
     // Remova uma task da listagem pelo ID
+    setTasks(
+      tasks.filter(i => i.id != id)
+    )
   }
 
   return (
